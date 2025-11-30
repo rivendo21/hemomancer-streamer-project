43dc5d39a3faed57e708644ba89515e8751ec6c1
rivendo21 <abdelrahman2729@gmail.com>
2025-11-30 02:57:05 +0200

    mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <nav className="navbar">
       <div className="left-section">
         <a href="/">
-          <img className="logo-img" src="hemomancer.png" />
+          <img className="logo-img" src="hemomancer.png" alt="Logo" />
         </a>
       </div>
-      <div className="right-section">
-        <NavLink to="/home">About</NavLink>
-        <NavLink to="/Leaderboard">Leaderboard</NavLink>
-        <NavLink to="/PatchNotes">Patch notes</NavLink>
+
+      <div className={`right-section ${menuOpen ? "open" : ""}`}>
+        <NavLink to="/home" onClick={() => setMenuOpen(false)}>
+          About
+        </NavLink>
+        <NavLink to="/Leaderboard" onClick={() => setMenuOpen(false)}>
+          Leaderboard
+        </NavLink>
+        <NavLink to="/PatchNotes" onClick={() => setMenuOpen(false)}>
+          Patch Notes
+        </NavLink>
+      </div>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
   );
